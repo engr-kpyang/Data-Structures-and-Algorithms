f1ae97890dacfbfd667cab49c7d265f059a2ef16
Kong Yang <kpyang2414@gmail.com>
2022-11-02 19:40:11 -0500

    Added basic queue

---
+++ src/DataStructures/queues.py
@@ -0,0 +1,36 @@
+# Queue Data structure
+
+class Queue:
+    def __init__(self) -> None:
+
+        self.queue  = []
+    
+    # Add an element
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    #Remove an element
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
+
+    # Display the queue
+    def displayQueue(self):
+        print(self.queue)
+
+    def queueSize(self):
+        return len(self.queue)
+
+    
+newQueue = Queue()
+
+newQueue.enqueue(47)
+newQueue.enqueue(99)
+newQueue.displayQueue()
+newQueue.enqueue(48)
+newQueue.enqueue(32)
+newQueue.displayQueue()
+newQueue.dequeue()
+newQueue.dequeue()
+newQueue.displayQueue()
--- src/DataStructures/stack.py
+++ src/DataStructures/stack.py
@@ -1,32 +1,38 @@
-# Stacks
+# Stack Data Structure
 
 def create_stack():
     stack = []
     return stack
 
-
-def check_empty(stack):
-    return len(stack) == 0
-
+def stack_peek(stack):
+        top = stack[-1]
+        return f'The current element on top is: {top}'   
 
 def push(stack, item):
     stack.append(item)
-    print('Pushed Item: ' + item)
+    print(f"Pushed item '{item}', into the stack.")
 
 
 def pop(stack):
-    if (check_empty(stack)):
+    if len(stack) == 0:
         return 'stack is empty'
-
+        
     return stack.pop()
 
-Stack = create_stack()
-push(Stack, 's')
-push(Stack, 't')
-push(Stack, 'a')
-push(Stack, 'c')
-push(Stack, 'k')
-push(Stack, str(99))
-print(Stack)
-pop(Stack)
-print(Stack)
+
+stack = create_stack()
+push(stack, 's')
+print(stack)
+push(stack, 't')
+print(stack)
+push(stack, 'a')
+print(stack_peek(stack))
+push(stack, 'c')
+push(stack, 'k')
+push(stack, str(99))
+print(stack)
+pop(stack)
+pop(stack)
+print(stack)
+
+
